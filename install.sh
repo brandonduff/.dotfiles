75826c54b5e40d338dd3e386d1783672b70dba0a
Brandon Duff <brandonduff17@gmail.com>
2019-01-16 08:24:49 -0700

    Add tmux conf

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.aliases ~/.aliases
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
