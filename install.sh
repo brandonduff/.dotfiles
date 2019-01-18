9b56b0e94b89c97561ee7ce3131d499072d0322a
brandonduff <bduff@mavenlink.com>
2019-01-18 16:55:21 -0700

    add tmuxinator to install file

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.aliases ~/.aliases
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+gem install tmuxinator
