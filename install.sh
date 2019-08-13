8102a2ff91fcf4f4456ca6d0902186bc89ff7dd5
brandonduff <bduff@mavenlink.com>
2019-08-13 09:20:59 -0600

    Add option to skip tmuxinator

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.aliases ~/.aliases
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-gem install tmuxinator
+if [ ! -z "$NO_TMUXINATOR" ]
+then
+	ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+	gem install tmuxinator
+fi
